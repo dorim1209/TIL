7bceba49e884ce36c9335a5fb3404abdf2531b37
dorim1209 <dorim011@naver.com>
2019-11-05 15:42:45 +0900

    Complete Node

--- Node/index.js
+++ Node/index.js
@@ -1,4 +1,4 @@
 const app = require("./app");
-const logger = require("./logger");
+require("./logger");
 
 app();
--- Node/logger.js
+++ Node/logger.js
@@ -1,15 +1,14 @@
 const url = "https://naver.com";
 
-console.log("이 코드는 한 번만 실행됩니다!");
+console.log("이 코드는 한 번만 실행 됩니다!");
 
 const log = msg => {
-  console.log(`Loggin Message : ${msg}`);
+  // Somethign with url;
+  console.log(`Logging Message : ${msg}`);
 };
 
 console.log("진짜루요!");
-
 module.exports = log;
 // module.exports.log = log;
 // exports.log = log;
-
-// exports = log; //module.exports에서는 아무것도 할당되지 않았습니다.
+//exports = log; // module.exports에는 아무것도 할당되지 않았습니다.
